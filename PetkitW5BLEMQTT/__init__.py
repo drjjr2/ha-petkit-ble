da6c29ccacffaa17a453ac6502f6dec4a01ff5f0
drjjr2 <69729977+drjjr2@users.noreply.github.com>
2026-08-21 14:24:26 -0500

    Delete PetkitW5BLEMQTT directory

--- PetkitW5BLEMQTT/__init__.py
+++
@@ -1,9 +0,0 @@
-from .ble_manager import BLEManager
-from .device import Device
-from .event_handlers import EventHandlers
-from .logger import Logger
-from .utils import Utils
-from .constants import Constants
-from .parsers import Parsers
-# Previously: MQTT modules (MQTTPayloads, MQTTClient, MQTTCallback) imported here
-from .commands import Commands
